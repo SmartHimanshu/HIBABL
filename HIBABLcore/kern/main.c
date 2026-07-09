f9f8c87992aa500928c94409f52f3042338eb26d
Himanshu Bakshi <himanshu.bakshi478@gmail.com>
2026-07-09 21:45:07 +0530

    Changed welcome msg

--- HIBABLcore/kern/main.c
+++ HIBABLcore/kern/main.c
@@ -5,7 +5,7 @@ void __attribute__ ((noreturn))
 kmain()
 {
     console_init();
-    console_print(console_make_color(VGA_WHITE, VGA_BLACK), "Hello world!");
-    
+    console_print(console_make_color(VGA_WHITE, VGA_BLACK), "Welcome to HIBA bootloader!");
+
     while(1) { };
 }
