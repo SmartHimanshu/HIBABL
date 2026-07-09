627ee78e46154644fc01e8c9685cb5c38ef64cb8
Himanshu Bakshi <himanshu.bakshi478@gmail.com>
2026-07-09 21:44:01 +0530

    Setting up things for mm

---
+++ HIBABLcore/kern/memory/mm.c
@@ -0,0 +1,8 @@
+/* This file is responsible for making a custom memory allocator */
+
+/*
+It's implementation is a simple linked list.
+We map 4MiB bytes initially and then we continue to 
+give malloc next 1MiB if we run out of space.
+
+*/
---
+++ HIBABLcore/kern/memory/mmap.c
@@ -0,0 +1,2 @@
+/* This file's main job is to load the memory map at a fixed offset so that kernel in future will
+take its pointer comfortably. */
