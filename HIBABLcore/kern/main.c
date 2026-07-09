d91090ad165c11a1b9f8ab748715d3ced569187e
Himanshu Bakshi <himanshu.bakshi478@gmail.com>
2026-07-09 16:41:05 +0530

    Final Fixes, Now we are able to comfortably call bios interrupts from c easily.

--- HIBABLcore/kern/main.c
+++ HIBABLcore/kern/main.c
@@ -4,13 +4,8 @@
 void __attribute__ ((noreturn))
 kmain()
 {
-    struct bios_regs regs = {0};
-
-    regs.eax = (0x0E << 8) | 'A';  // AH=0Eh, AL='A'
-    regs.ebx = 0;                  // BH=0, BL=0
-
-    bios_interrupt(0x10, &regs);
-
+    console_init();
+    console_print(console_make_color(VGA_WHITE, VGA_BLACK), "Hello world!");
 
     while(1) { };
 }
